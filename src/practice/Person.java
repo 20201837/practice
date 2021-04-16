64ce8d9e3d02cd09fca908a3d5d442c4bdd29a43
20201837fy <2848718426@qq.com>
2021-04-16 17:11:18 +0800

    初始创建

---
+++ src/practice/Person.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Person {
+    void speakHello() {
+
+        System.out.println("nice to meet you");
+    }
+}
+
+class Xiti{
+    public static void main(String args[]){
+        Person zhang=new Person() ;
+        zhang.speakHello();
+    }
+}
---
+++ src/practice/Vehicle.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class Vehicle {
+    double speed;
+    int power;
+    void speedUp(int  s) {
+        speed=speed+s;
+        }
+    void speedDown(int d) {
+            speed = speed - d;
+        }
+        void setPower(int p){
+        power=p;
+    }
+    int getPower(){
+        return power;
+    }
+    double getSpeed(){
+        return speed;
+    }
+    void brake() {
+        speed=0;
+    }
+}
+
+
